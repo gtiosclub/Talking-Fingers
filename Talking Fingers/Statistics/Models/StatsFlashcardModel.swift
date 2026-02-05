cbe41b6ae763b70bd962ab38114ff5581f9030aa
Ethan Kim <jikim1803@gmail.com>
2026-02-05 18:42:30 -0500

    comment out buggy file

--- Talking Fingers/Statistics/Models/StatsFlashcardModel.swift
+++ Talking Fingers/Statistics/Models/StatsFlashcardModel.swift
@@ -1,24 +1,24 @@
+////
+////  Flashcard.swift
+////  Talking Fingers
+////
+////  Created by Krish Prasad on 2/4/26.
+////
+//import SwiftData
+//import Foundation
 //
-//  Flashcard.swift
-//  Talking Fingers
-//
-//  Created by Krish Prasad on 2/4/26.
-//
-import SwiftData
-import Foundation
-
-@Model
-class StatsFlashcard {
-    @Attribute(.unique) var term: String
-    var definition: String
-    var lastSucceeded: Date?
-    var starred: Bool
-    var progress: ProgressType
-    
-    init(term: String, definition: String, lastSucceeded: Date? = nil, starred: Bool) {
-        self.term = term
-        self.definition = definition
-        self.lastSucceeded = nil
-        self.starred = false
-    }
-}
+//@Model
+//class StatsFlashcard {
+//    @Attribute(.unique) var term: String
+//    var definition: String
+//    var lastSucceeded: Date?
+//    var starred: Bool
+//    var progress: ProgressType
+//    
+//    init(term: String, definition: String, lastSucceeded: Date? = nil, starred: Bool) {
+//        self.term = term
+//        self.definition = definition
+//        self.lastSucceeded = nil
+//        self.starred = false
+//    }
+//}
