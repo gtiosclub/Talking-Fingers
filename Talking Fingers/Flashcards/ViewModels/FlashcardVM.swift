337f4ad1428339393327175df20ffe35cfaf25e1
sadusumilli36 <sanvi.adusumilli@gmail.com>
2026-02-12 18:48:55 -0500

    Return progress towards mastery.

--- Talking Fingers/Flashcards/ViewModels/FlashcardVM.swift
+++ Talking Fingers/Flashcards/ViewModels/FlashcardVM.swift
@@ -12,5 +12,23 @@ import Combine
 class FlashcardVM {
 
     @Published var flashcards: [FlashcardModel] = []
-
+    
+    func returnProgress(flashcards: [FlashcardModel]) -> Float {
+        guard !flashcards.isEmpty else { return 0.0 }
+        var progressTotal: Float = 0.0
+        
+        for flashcard in flashcards {
+            switch flashcard.progress {
+            case .new:
+                progressTotal += 0
+            case .learning:
+                progressTotal += 30
+            case .polishing:
+                progressTotal += 70
+            case .mastered:
+                progressTotal += 100
+            }
+        }
+        return progressTotal / Float(flashcards.count)
+    }
 }
