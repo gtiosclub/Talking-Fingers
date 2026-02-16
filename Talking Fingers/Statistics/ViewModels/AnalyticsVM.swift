48951a5b1d2d205bc16eddc726ed4b82b4ad314b
Krish Prasad <krishkp00@gmail.com>
2026-02-16 16:23:16 -0500

    Create AnalyticsVM.swift

---
+++ Talking Fingers/Statistics/ViewModels/AnalyticsVM.swift
@@ -0,0 +1,15 @@
+//
+//  AnalyticsVM.swift
+//  Talking Fingers
+//
+//  Created by Krish Prasad on 2/16/26.
+//
+
+import Observation
+import SwiftData
+import Foundation
+
+@Observable
+class AnalyticsVM {
+    
+}
