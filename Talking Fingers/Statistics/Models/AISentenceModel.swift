393aef270b5349faf0ef87438706cecdf6b6817c
Krish Prasad <krishkp00@gmail.com>
2026-02-15 22:35:15 -0500

    update

--- Talking Fingers/Statistics/Models/AISentenceModel.swift
+++ Talking Fingers/Statistics/Models/AISentenceModel.swift
@@ -8,20 +8,30 @@
 import Foundation
 
 enum PracticeType: String, Codable {
-    case words
-    case signs
+    case words = "word"
+    case signs = "signs"
+}
+
+enum Difficulty: String, Codable {
+    case easy = "easy"
+    case medium = "medium"
+    case hard = "hard"
 }
 
 struct AISentenceModel: Identifiable, Codable {
     var id = UUID()
-    var words: [FlashcardModel]
+    var words: [String]
+    var gloss: [String]
     var score: [Int]
     var practiceType: PracticeType
+    var difficulty: Difficulty
 
-    init(words: [FlashcardModel], score: [Int], practiceType: PracticeType) {
+    init(words: [String], score: [Int], practiceType: PracticeType, difficulty: Difficulty, gloss: [String]) {
         self.id = UUID()
         self.words = words
         self.score = score
         self.practiceType = practiceType
+        self.difficulty = difficulty
+        self.gloss = gloss
     }
 }
