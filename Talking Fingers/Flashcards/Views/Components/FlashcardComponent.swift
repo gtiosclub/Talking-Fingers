ef58717b2c9a9a540e132d7568f81edf145f80fe
nahua3730 <nahua3730@gmail.com>
2026-02-16 19:27:57 -0500

    Implement static diagram linking via UUID

--- Talking Fingers/Flashcards/Views/Components/FlashcardComponent.swift
+++ Talking Fingers/Flashcards/Views/Components/FlashcardComponent.swift
@@ -8,11 +8,28 @@
 import SwiftUI
 
 struct FlashcardComponent: View {
+    var card: FlashcardModel
     var body: some View {
+        VStack{
+            Text(card.term)
+            if let image = UIImage(named: card.id.uuidString) {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 250)
+            }
+        }
         Text("this is a flashcard component")
     }
 }
-
+    
 #Preview {
-    FlashcardComponent()
+    FlashcardComponent(
+        card: FlashcardModel(
+            term: "Apple",
+            id: UUID(uuidString: "11111111-1111-1111-1111-111111111111")!,
+            category: "Test"
+        )
+    )
 }
+
