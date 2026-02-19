7164b16f31e251d0be23a5b3fadff52ea48dd354
jsachdeva7 <jsachdeva7@gatech.edu>
2026-02-19 17:35:13 -0500

    progress view

---
+++ Talking Fingers/Statistics/Views/ProgressView.swift
@@ -0,0 +1,18 @@
+//
+//  ProgressView.swift
+//  Talking Fingers
+//
+//  Created by Jagat Sachdeva on 2/19/26.
+//
+
+import SwiftUI
+
+struct ProgressView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProgressView()
+}
