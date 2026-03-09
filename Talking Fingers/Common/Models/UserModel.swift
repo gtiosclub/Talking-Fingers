7950431a1670e29ba88e4b3f87c5b3562ebe8653
sadusumilli36 <sanvi.adusumilli@gmail.com>
2026-03-09 19:00:46 -0400

    Fixing bug with birthday field

--- Talking Fingers/Common/Models/UserModel.swift
+++ Talking Fingers/Common/Models/UserModel.swift
@@ -14,14 +14,15 @@ class User {
     var password: String
     var birthday: Date
     var flashcards: [FlashcardModel]
-    var unlockedCategories: [String] //change to [Category] later
+    var unlockedCategories: [String]
     
-    init(userId: String, name: String, email: String, birthday: Date) {
+    init(userId: String, name: String, email: String) {
         self.userId = userId
         self.name = name
         self.email = email
+        
         self.password = ""
-        self.birthday = birthday
+        self.birthday = Date()
         self.flashcards = []
         self.unlockedCategories = []
     }
