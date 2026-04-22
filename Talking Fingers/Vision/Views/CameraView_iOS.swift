5529891be541e52e0b0cf22f6695ed03ffb15dd3
nikola-cao <cao.nikola.a@gmail.com>
2026-04-21 23:40:25 -0400

    updated threshold values

--- Talking Fingers/Vision/Views/CameraView_iOS.swift
+++ Talking Fingers/Vision/Views/CameraView_iOS.swift
@@ -516,8 +516,8 @@ struct CameraView: View {
 
     private var confidenceColor: Color {
         let score = cameraVM.confidenceScore
-        let goodThreshold: Double = cameraVM.activeComparisonType == .static ? 62 : 50
-        let okayThreshold: Double = cameraVM.activeComparisonType == .static ? 46 : 27
+        let goodThreshold: Double = cameraVM.activeComparisonType == .static ? 80 : 70
+        let okayThreshold: Double = cameraVM.activeComparisonType == .static ? 60 : 51
         if score >= goodThreshold { return .green }
         if score >= okayThreshold { return .yellow }
         return .red
@@ -525,8 +525,8 @@ struct CameraView: View {
 
     private var confidenceLabel: String {
         let score = cameraVM.confidenceScore
-        let goodThreshold: Double = cameraVM.activeComparisonType == .static ? 62 : 50
-        let okayThreshold: Double = cameraVM.activeComparisonType == .static ? 46 : 27
+        let goodThreshold: Double = cameraVM.activeComparisonType == .static ? 80 : 70
+        let okayThreshold: Double = cameraVM.activeComparisonType == .static ? 60 : 51
         if score >= goodThreshold { return "Good" }
         if score >= okayThreshold { return "Okay" }
         return "Bad"
